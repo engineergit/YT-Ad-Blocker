34e3561954de4545355d6ac2f1d063944f2fa1ae
Sohail <sohailmalik059@gmail.com>
2022-12-28 20:03:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    let skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+    if (skipButton != undefined && skipButton.length > 0) {
+        console.log('ad detected');
+        skipButton[0].click();
+    }   
+}, 3000);
