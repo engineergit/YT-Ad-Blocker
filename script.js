c2d12a98969968a8fa63072fa0ddb89f87c0b741
Sohail <sohailmalik059@gmail.com>
2022-12-29 20:51:37 +0500

    optimzied code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+
 setInterval(() => {
-    let skipButton = document.getElementsByClassName("ytp-ad-skip-button");
     if (skipButton != undefined && skipButton.length > 0) {
         console.log('ad detected');
         skipButton[0].click();
     }   
-}, 3000);
+}, 1000); // check for ad every 1000 milliseconds (1 second)
